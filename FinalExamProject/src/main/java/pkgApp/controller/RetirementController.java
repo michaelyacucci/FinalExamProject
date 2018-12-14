edae554062e89151c581d4d57c86dda05ff216c7
micha <micha@128.4.56.175>
2018-12-13 21:44:25 -0500

    fixed an import i guess

--- FinalExamProject/src/main/java/pkgApp/controller/RetirementController.java
+++ FinalExamProject/src/main/java/pkgApp/controller/RetirementController.java
@@ -1,12 +1,12 @@
 package pkgApp.controller;
 
-import java.awt.Label;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import pkgApp.RetirementApp;
 import pkgCore.Retirement;
